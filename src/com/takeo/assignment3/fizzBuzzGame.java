59a877820910ffc522a77292d6fe2c680b9dc3c1
SudipD10 <sudipdharel1111@gmail.com>
2023-07-24 10:52:08 -0500

    Second commit

--- src/com/takeo/assignment3/fizzBuzzGame.java
+++ src/com/takeo/assignment3/fizzBuzzGame.java
@@ -1,4 +1,24 @@
 package com.takeo.assignment3;
 
+import java.util.Scanner;
+
 public class fizzBuzzGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter your Number ");
+        int limit = scanner.nextInt();
+
+        for(int i = 1; i <= limit; i++){
+            if(i % 15 == 0 ) {
+                System.out.println(" fizzBuzz ");
+            } else if( i % 3 == 0 ) {
+                System.out.println(" Fizz ");
+            } else if( i % 5 == 0 ) {
+                System.out.println(" Buzz ");
+            } else{
+                System.out.println( i );
+            }
+        }
+
+    }
 }
